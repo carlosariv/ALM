af29e59a332752c01828644a175aa9ef1d65756b
Carlos Rivera <carlosariv@outlook.com>
2026-05-17 23:04:18 -0500

    Resolver, symbol resolution

--- src/Resolve.cpp
+++ src/Resolve.cpp
@@ -1,16 +1,78 @@
+#include <assert.h>
+#include <print>
+
+#include "Atom.h"
 #include "Ast.h"
 #include "Resolve.h"
+
+
+//NOTE: Use for hasing atoms
+// uint64_t hash_pointer(void* ptr) {
+//     uint64_t x = (uintptr_t)ptr;
+//     x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//     x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//     x = x ^ (x >> 31);
+//     return x;
+// }
+
+template<typename... Args>
+void report_error(Ast *node, std::format_string<Args...> fmt, Args&&... args) {
+    std::print("error: ");
+    std::println(fmt, std::forward<Args>(args)...);
+}
+
+void resolve_name(Resolver *R, AstName *name) {
+}
 
 void resolve_expr(Resolver *R, Ast *expr) {
     switch (expr->kind) {
         default:
             break;
+
+        case Ast_Name: {
+            AstName *name = static_cast<AstName*>(expr);
+            resolve_name(R, name);
+            break;
+        }
+
+        case Ast_LiteralExpr: {
+            LiteralExpr *le = static_cast<LiteralExpr*>(expr);
+            break;
+        }
     }
+}
+
+Symbol *symbol_find(Resolver *R, Atom *name) {
+    Scope *scope = R->scope;
+    for (Symbol *s : scope->symbol_table) {
+        if (s->name == name) {
+            return s;
+        }
+    }
+    return nullptr;
+}
+
+Symbol *symbol_create(Resolver *R) {
+    Symbol *s = new Symbol;
+    s->scope = R->scope;
+    R->scope->symbol_table.add(s);
+    return s;
 }
 
 void resolve_value_decl(Resolver *R, AstValueDecl *vd) {
     for (Ast *expr : vd->lhs) {
-        resolve_expr(R, expr);
+        assert(expr->kind == Ast_Name);
+        AstName *name = static_cast<AstName*>(expr);
+
+        Symbol *lookup = symbol_find(R, name->name);
+        if (lookup) {
+            report_error(name, "found duplicate of '{}'", get_string(name->name));
+        } else {
+            Symbol *symbol = symbol_create(R);
+            symbol->name = name->name;
+            symbol->node = name;
+            name->symbol = symbol;
+        }
     }
 
     for (Ast *expr : vd->rhs) {
@@ -26,7 +88,6 @@ void resolve_value_decl(Resolver *R, AstValueDecl *vd) {
             // }
         }
     }
-
 }
 
 void resolve_top_level_stmt(Resolver *R, Ast *stmt) {
@@ -41,6 +102,8 @@ void resolve_top_level_stmt(Resolver *R, Ast *stmt) {
 
 
 void resolve_file(Resolver *R, AstFile *file) {
+    Scope *global_scope = new Scope;
+    R->scope = global_scope;
     for (Ast *stmt : file->decls) {
         resolve_top_level_stmt(R, stmt);
     }
--- src/Resolve.h
+++ src/Resolve.h
@@ -1,4 +1,22 @@
 #pragma once
 
-struct Resolver {
+struct AstFile;
+struct Scope;
+
+struct Symbol {
+    Atom *name;
+    Ast *node;
+    Scope *scope;
 };
+
+struct Scope {
+    Scope *parent = nullptr;
+    Array<Symbol*> symbol_table;
+};
+
+struct Resolver {
+    Scope *scope = nullptr;
+};
+
+
+void resolve_file(Resolver *R, AstFile *file);
