f0d4f5a04d366852d66fd7ae4dfcd7ae7513ba01
Carlos Rivera <carlosariv@outlook.com>
2026-05-17 23:04:46 -0500

    Types

---
+++ src/Types.cpp
@@ -0,0 +1,29 @@
+
+Type builtin_types[] = {
+    {Type_Unknown, STRZ("<unknown>"), 0},
+    {Type_Void,    STRZ("void"),      0},
+    {Type_Bool,    STRZ("bool"),      1},
+    {Type_U8,      STRZ("u8"),        1},
+    {Type_U16,     STRZ("u16"),       2},
+    {Type_U32,     STRZ("u32"),       4},
+    {Type_U64,     STRZ("u64"),       8},
+    {Type_I8,      STRZ("i8"),        1},
+    {Type_I16,     STRZ("i16"),       2},
+    {Type_I32,     STRZ("i32"),       4},
+    {Type_I64,     STRZ("i64"),       8},
+    {Type_F32,     STRZ("f32"),       4},
+    {Type_F64,     STRZ("f64"),       8},
+};
+
+Type *t_void = &builtin_types[Type_Void];
+Type *t_bool = &builtin_types[Type_Bool];
+Type *t_u8 = &builtin_types[Type_U8];
+Type *t_u16 &builtin_types[Type_U16];
+Type *t_u32 = &builtin_types[Type_U32];
+Type *t_u64 = &builtin_types[Type_U64];
+Type *t_i8 = &builtin_types[Type_I8];
+Type *t_i16 &builtin_types[Type_I16];
+Type *t_i32 = &builtin_types[Type_I32];
+Type *t_i64 = &builtin_types[Type_I64];
+Type *t_f32 = &builtin_types[Type_F32];
+Type *t_f64 = &builtin_types[Type_F64];
---
+++ src/Types.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "Array.h"
+
+enum TypeKind {
+    Type_Unknown,
+    Type_Void,
+    Type_Bool,
+    Type_U8,
+    Type_U16,
+    Type_U32,
+    Type_U64,
+    Type_I8,
+    Type_I16,
+    Type_I32,
+    Type_I64,
+    Type_F32,
+    Type_F64,
+
+    Type_Pointer,
+    Type_Array,
+    Type_Any,
+    Type_Enum,
+    Type_Struct,
+    Type_Union,
+    Type_Procedure,
+    Type_Tuple,
+    Type_String,
+
+    Type_COUNT
+};
+
+struct Type {
+    TypeKind kind = Type_Unknown;
+    String name;
+    int bytes;
+};
+
+struct PointerType : Type {
+    Type *base;
+
+    PointerType() {
+        kind = Type_Pointer;
+    }
+};
+
+struct ArrayType : Type {
+    Type *base;
+    CompileTimeValue fixed_size;
+
+    ArrayType() {
+        kind = Type_Array;
+    }
+};
+
+struct ProcedureType : Type {
+    Type *argument_type;
+    Type *return_type;
+
+    ProcedureType() {
+        kind = Type_Procedure;
+    }
+};
+
+struct TupleType : Type {
+    Array<Type*> types;
+    TupleType() {
+        kind = Type_Tuple;
+    }
+};
+
+
+extern Type *t_void;
+extern Type *t_bool;
+extern Type *t_u8;
+extern Type *t_u16;
+extern Type *t_u32;
+extern Type *t_u64;
+extern Type *t_i8;
+extern Type *t_i16;
+extern Type *t_i32;
+extern Type *t_i64;
+extern Type *t_f32;
+extern Type *t_f64;
